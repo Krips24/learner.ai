44e2e216b7c60c3a0dcd180a04ae68f74fe50217
Krips24 <krapansh24@gmail.com>
2025-12-09 16:18:35 +0530

    env var added in AWS Amplify

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Footer } from "@/components/footer";
 import { Hero } from "@/components/hero";
 import { HowItWorks } from "@/components/howitworks";
 import { Navbar } from "@/components/navbar";
-
+ 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col bg-background text-foreground">
